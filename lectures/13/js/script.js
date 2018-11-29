c95b4f8d8f3ed33f257af6f598906dbdb1a29bde
noor ahmad <noorahmad1990@gmail.com>
2018-11-29 20:15:41 +0500

    quiz javascript

--- lectures/13/js/script.js
+++ lectures/13/js/script.js
@@ -20,29 +20,96 @@ var questions = [{
         "none of the above"],
     correctAnswer : 1
 }];
-
+var index=-1;
 var currentQuestion = 0;
 var correctAnswers = 0;
 var quizOver = false;
-displayCurrentQuestion();
+
+var div = document.getElementById("question");
+var p = document.createElement("p");
+var li=document.createElement("li");
+var li1=document.createElement("li");
+var li2=document.createElement("li");
+var li3=document.createElement("li");
+
+var ul=document.getElementById("choice-list");
+
+
 document.getElementById("quiz-message").style.display = 'none';
+//displayCurrentQuestion();
+
 function displayNext() {
     /*Write your code here */
+    if(index>0 && index < questions.length) {
+        if (document.getElementById("r1").checked) {
+            if (document.getElementById("r1").value == questions[index].correctAnswer) {
+                correctAnswers++;
+            }
+        }
+        else if (document.getElementById("r2").checked) {
+            if (document.getElementById("r2").value == questions[index].correctAnswer) {
+                correctAnswers++;
+            }
+        }
+        else if (document.getElementById("r3").checked) {
+            if (document.getElementById("r3").value == questions[index].correctAnswer) {
+                correctAnswers++;
+            }
+        }
+        else if (document.getElementById("r4").checked) {
+            if (document.getElementById("r4").value == questions[index].correctAnswer) {
+                correctAnswers++;
+            }
+        }
+    }
+    console.log("correct answr value: "+ correctAnswers);
+    console.log("ques:" + questions.length);
+    console.log("in click "+index);
+    index++;
+    if(index < questions.length){
+        console.log("index: "+index);
+        displayCurrentQuestion();
+        div.clear;
+        ul.clear;
+        hideScore();
+    }
+    else if(index==3){
+        console.log("in else");
+        displayScore();
+        resetQuiz();
+    }
 }
 
 function displayCurrentQuestion() {
     /*Write your code here */
+    p.innerText=questions[index].question;
+    div.appendChild(p);
+
+     li.innerHTML='<input id="r1" type="radio" name="answer" value=0>'+questions[index].choices[0];
+
+    li1.innerHTML='<input id="r2" type="radio" name="answer" value=1>'+questions[index].choices[1];
+
+    li2.innerHTML='<input id="r3" type="radio" name="answer" value=2>'+questions[index].choices[2];
+
+    li3.innerHTML='<input id="r4" type="radio" name="answer" value=3>'+questions[index].choices[3];
+
+    ul.appendChild(li);
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
 }
 
 function resetQuiz() {
+    console.log("in reset quiz");
     currentQuestion = 0;
+    index=-1;
     correctAnswers = 0;
-    hideScore();
 }
 function displayScore() {
-    document.getElementById("result").innerHTML = "you scored: " + correctAnswers + " out of: " + questions.length;
+    document.getElementById("result").innerText = "you scored: " + correctAnswers + " out of: " + questions.length;
     document.getElementById("result").style.display = 'block';
 }
 function hideScore() {
     document.getElementById("result").style.display = 'none';
+    console.log("in hide score");
 }
